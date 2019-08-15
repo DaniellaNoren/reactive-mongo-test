c84d9c20f011ca954be22b12bfbcb8d24b389f28
Daniella Noren <daniellanoren@live.se>
2019-08-15 20:32:40 +0200

    Initial commit

---
+++ src/main/java/com/daniella/reactive/reactivemongotest/ReactiveMongoTestApplication.java
@@ -0,0 +1,13 @@
+package com.daniella.reactive.reactivemongotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoTestApplication.class, args);
+	}
+
+}
